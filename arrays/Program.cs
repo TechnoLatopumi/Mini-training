3b5a795726bdd7f8700dc1f9619f919fd1c01197
TechnoLatopumi <tegartpp@gmail.com>
2022-08-04 13:51:10 +0700

    added mad-lib

---
+++ arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int [] luckyNumbers = {4, 7, 40, 32, 15, 13, 22};
+
+        luckyNumbers[1] = 700;
+
+        Console.WriteLine( luckyNumbers[1]);
+
+        Console.ReadLine();
+    }
+}
