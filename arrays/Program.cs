bf7febe8bd7165c980ba30a7ae23c717bf10d2f8
TechnoLatopumi <tegartpp@gmail.com>
2022-08-04 20:19:09 +0700

    added Array

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -4,7 +4,11 @@
     {
         int [] luckyNumbers = {4, 7, 40, 32, 15, 13, 22};
 
-        luckyNumbers[1] = 700;
+        string[] friends = new string[5];
+        friends[0] = "Jim";
+        friends[1] = "Kelly";
+        
+
 
         Console.WriteLine( luckyNumbers[1]);
 
