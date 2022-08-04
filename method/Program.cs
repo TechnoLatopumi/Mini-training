20784520c3bfe7e029c6300c2471f5e384e1e032
TechnoLatopumi <tegartpp@gmail.com>
2022-08-04 20:35:20 +0700

    Added Methods

---
+++ method/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayHi("Tegar", 29);
+        SayHi("Dimas", 28);
+        SayHi("Pradana", 30);
+        Console.ReadLine();
+
+    }
+
+    static void SayHi(string name, int age)
+    {
+
+        Console.WriteLine("Hello "+name+" you are "+age);
+    }
+}
---
+++ method/README.md
@@ -0,0 +1,1 @@
+Method explanation from FreeCodeCamp.org
