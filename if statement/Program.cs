86471c2965816e009ea75a3b279160da34d783a2
TechnoLatopumi <tegartpp@gmail.com>
2022-08-05 08:11:48 +0700

    updated if statement

--- if statement/Program.cs
+++ if statement/Program.cs
@@ -3,13 +3,16 @@
     private static void Main(string[] args)
     {
         bool isMale = false;
-        bool isTall = true ;
-        if (isMale || isTall)
+        bool isTall = true;
+        if (isMale && isTall)
         {
-            Console.WriteLine("You are probably tall or male or both");
-        } else
-        {
-            Console.WriteLine("YOu are not tall and not a male");
+            Console.WriteLine("You are a tall male");
+        } else if (isMale && !isTall) {
+            Console.WriteLine("You are a male and not tall");
+        } else if (!isMale && isTall) {
+            Console.WriteLine("You are tall and not a male");
+        } else {
+            Console.WriteLine("You are not tall and not a male");
         }
         Console.ReadLine();
     }
