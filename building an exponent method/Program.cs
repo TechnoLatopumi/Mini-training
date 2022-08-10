c48df0566eadc3d47e1f2387bf992c76a6dbee70
TechnoLatopumi <tegartpp@gmail.com>
2022-08-10 07:40:35 +0700

    added exponent method

---
+++ building an exponent method/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetPow(3,3));
+        Console.ReadLine();
+    }
+    static int GetPow(int baseNum, int powNum)
+    {
+        int result =1 ;
+        for(int i = 0; i <powNum; i++)
+        {
+            result = result*baseNum ;
+        }
+
+        return result;
+    }
+}
