ac81445f3ee046819dadc42b7890123d8df507e3
TechnoLatopumi <tegartpp@gmail.com>
2022-08-10 07:28:14 +0700

    added For loops

---
+++ for loops/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int [] listNumbers = {4,5,3,9,7,1,4,6,8};
+        for (int i = 0; i < listNumbers.Length; i++)
+        {
+            Console.WriteLine(listNumbers[i]);
+        }
+        Console.ReadLine();
+    }
+}
