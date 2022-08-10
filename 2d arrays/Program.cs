4fb3b07b6cacff095630da22519ce0f439b71f93
TechnoLatopumi <tegartpp@gmail.com>
2022-08-10 07:47:35 +0700

    added 2d arrays

---
+++ 2d arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int [,] numberGrid = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+        int[,] myArray = new int[2,3];
+        Console.WriteLine(numberGrid[2, 1]);
+        Console.ReadLine();
+    }
+}
