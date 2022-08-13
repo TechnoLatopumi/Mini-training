4c0518a18fe28ec6a0f227e2b183ea4e237740de
TechnoLatopumi <tegartpp@gmail.com>
2022-08-13 10:30:45 +0700

    added setters and getters

---
+++ getters and setters/Program.cs
@@ -0,0 +1,15 @@
+﻿using Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movie runner = new Movie("runner", "Jim", "unknown");
+        Movie transformer = new Movie("transformer", "Autobot", "R");
+        // G, PG, PG-13, R, NR
+        transformer.Rating = "chicken";
+        Console.WriteLine(runner.Rating);
+        Console.WriteLine(transformer.Rating);
+        Console.ReadLine();
+    }
+}
