84474f3e00a1a1a452d0af8b6f8fccb5dce82607
TechnoLatopumi <tegartpp@gmail.com>
2022-08-13 09:08:55 +0700

    added objects methods

---
+++ objectmethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student("Jim", "Business", 2.8);
+        Student student2 = new Student("Carl", "Compsci", 3.2);
+        Console.WriteLine(student1.GoodGpa());
+        Console.WriteLine(student2.GoodGpa());
+        Console.ReadLine();
+    }
+}
