d884b19525a6b99b90024903ebf6b9465f8ebec4
TechnoLatopumi <tegartpp@gmail.com>
2022-08-13 10:52:00 +0700

    added static class atribute

---
+++ static class atribute/Program.cs
@@ -0,0 +1,16 @@
+﻿using Classes;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Song holiday =  new Song("Blast", "Jim", 200);
+        Console.WriteLine(Song.songCount);
+        Song fladed =  new Song("Fladed", "Carl", 300);
+        Console.WriteLine(fladed.getSongCount());
+   
+
+        Console.ReadLine();
+    }
+}
---
+++ static class atribute/Song.cs
@@ -0,0 +1,23 @@
+namespace Classes;
+
+class Song
+{
+    public string title;
+    public string artist;
+    public int duration;
+
+    public static int songCount = 0;
+
+    public Song (string title, string artist, int duration)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        songCount++;
+    }
+
+    public int getSongCount ()
+    {
+        return songCount ;
+    }
+}
